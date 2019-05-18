0045afaaa898aa6078d5fe96ede9415c62bf1823
Mennad Yami <mennadyami@hotmail.com>
2019-05-19 00:20:25 +0200

    Add file reading and parsing, handle directories

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,56 @@
-use std::path::Path;
+use std::error::Error;
+use std::path::PathBuf;
 
 // pub mod code_writer;
 pub mod parser;
 
-fn get_file_path(file_path: &str) -> Path {
-	let resolved = Path::new(file_path);
-	println!("{}", resolved);
-	resolved
+#[derive(Debug)]
+pub struct FileData {
+	destination_name: String,
+	original_path: PathBuf,
+	destination_path: PathBuf,
+}
+/// Get file information for a file path
+fn get_file_data(original_path: PathBuf) -> FileData {
+	let destination_path: PathBuf = original_path.with_extension("asm");
+	FileData {
+		destination_name: destination_path
+			.file_name()
+			.unwrap()
+			.to_os_string()
+			.into_string()
+			.unwrap(),
+		original_path,
+		destination_path,
+	}
 }
 
-pub fn translate(file_path: String) -> Result<(), Box<dyn std::error::Error>> {
-	// let original_path = get_file_path(&file_path);
-	// println!("{}", original_path);
-	get_file_path(&file_path);
+/// Returns a vector with every FileFata objects
+fn get_data(path: &str) -> Result<Vec<FileData>, Box<dyn Error>> {
+	let to_check = PathBuf::from(path);
+	let mut data = Vec::new();
+	if to_check.is_dir() {
+		for entry in to_check.read_dir()? {
+			if let Ok(entry) = entry {
+				data.push(get_file_data(entry.path()));
+			}
+		}
+	} else {
+		data.push(get_file_data(PathBuf::from(path)));
+	}
+	Ok(data)
+}
+
+pub fn translate(raw_path: String) -> Result<(), Box<dyn std::error::Error>> {
+	let files_data = get_data(&raw_path)?;
+	for file in files_data {
+		// let code_writer = code_writer::CodeWriter::new();
+		let content = parser::read_content(file)?;
+		let mut parser = parser::Parser::new(&content)?;
+		println!("{:?}", parser);
+		while parser.has_more_commands() {
+			parser.advance();
+		}
+	}
 	Ok(())
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,1 +1,49 @@
+use super::FileData;
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+use std::iter::Peekable;
+use std::str::Lines;
 
+pub fn read_content(file_data: FileData) -> Result<String, Box<dyn Error>> {
+	let mut file = File::open(file_data.original_path)?;
+	let mut content = String::new();
+	file.read_to_string(&mut content)?;
+	Ok(content)
+}
+
+#[derive(Debug)]
+pub struct Parser<'a> {
+	lines: Peekable<Lines<'a>>,
+	current_line: String,
+}
+
+impl<'a> Parser<'a> {
+	pub fn new(content: &'a str) -> Result<Self, Box<dyn Error>> {
+		let parser = Parser {
+			lines: content.lines().peekable(),
+			current_line: String::new(),
+		};
+		Ok(parser)
+	}
+
+	pub fn has_more_commands(&mut self) -> bool {
+		self.lines.peek() != None
+	}
+
+	pub fn advance(&mut self) {
+		self.current_line = self.lines.next().unwrap().to_owned();
+	}
+
+	pub fn command_type(&self) -> &str {
+		&self.current_line
+	}
+
+	pub fn arg_1(&self) -> &str {
+		&self.current_line
+	}
+
+	pub fn arg_2(&self) -> &str {
+		&self.current_line
+	}
+}
